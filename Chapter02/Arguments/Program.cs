240428c157de75b784d505f1eb0af0e97fda7bf0
unknown <zalmanren@zalmanren.com>
2021-08-18 19:15:33 +0800

    Arguments

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+// WriteLine($"There are {args.Length} arguments.");
+// foreach(var arg in args)
+//     Console.WriteLine(arg);
+
+// use parameters to changed the background color and foreground color
+if(args.Length < 4)
+{
+    WriteLine("You must specify two colors and dimensions, e.g.");
+    WriteLine("dotnet run red yellow 80 40");
+    return;
+}
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0],
+    ignoreCase: true
+);
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1],
+    ignoreCase: true
+);
+WindowWidth = int.Parse(args[2]);
+WindowHeight = int.Parse(args[3]);
+
+Console.WriteLine("Setting Done.");
---
+++ Chapter02/StaticConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console; // 减少以后无需输入Console, 而且现在忽略了命名空间，因为本身都是static的原因
+// See https://aka.ms/new-console-template for more information
+WriteLine("Hello, World!");
+var address = new Addres();
+Console.WriteLine(address.City);
+
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}", 
+    arg0:key.Key, 
+    arg1:key.KeyChar,
+    arg2:key.Modifiers);
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}", 
+    key.Key, 
+    key.KeyChar,
+    key.Modifiers);
+class Addres
+{
+    public string City = string.Empty;
+}

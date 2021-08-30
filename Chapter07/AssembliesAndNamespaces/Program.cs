dd118840556ec12624bc10f837aebf6f5bea0181
unknown <zalmanren@zalmanren.com>
2021-08-30 17:37:26 +0800

    Chapter 07

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Xml.Linq;
+using static System.Console;
+
+
+var doc = new XDocument();
+string s1 = "Hello"; 
+string s2 = "World";
+
+SharedLibrary.Class1.FunctionTest();
